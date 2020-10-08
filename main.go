6bc443fd97415fff22233a545ef7a3c1b47e167e
Kiet T. Tran, Ph.D <ktran@harrisburgu.edu>
2020-10-08 05:28:43 -0400

    Added readme.md

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {fmt.Fprint(w, "hello from web page")})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "hello from web page")
+	})
 	http.ListenAndServe(":8080", nil)
 }
